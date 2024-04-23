91ba28b184bbe8432137d14a5bcaf6534b3190ab
Melvyn <melvynhoarau@hotmail.fr>
2024-04-23 12:48:43 +0200

    apos

--- pages/success.js
+++ pages/success.js
@@ -7,9 +7,9 @@ export default function SuccessPage() {
         <div className="text-center mx-auto font-article">
             <GrStatusGood className="justify-center mx-auto text-6xl text-green-600" />
             <p className="text-2xl p-2">Paiement validé !</p>
-            <p className="p-1">Votre commande a bien été prise en compte, vous recevrez un mail de confirmation d'ici quelques heures.</p>
+            <p className="p-1">Votre commande a bien été prise en compte, vous recevrez un mail de confirmation d&apos;ici quelques heures.</p>
             <p className="p-1 mb-6 font-bold text-gray-800">À très bientôt !</p>
-            <Link href={'/'} className="bg-red-300 p-2 text-white">RETOUR À L'ACCUEIL</Link>
+            <Link href={'/'} className="bg-red-300 p-2 text-white">RETOUR À L&apos;ACCUEIL</Link>
         </div>
     )
 }
