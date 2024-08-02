7366cc91ceca7c1f6acd206d8300846188f23cee
Melvyn <melvynhoarau@hotmail.fr>
2024-08-02 20:01:49 +0200

    4.1

--- pages/composants/Avis.js
+++ pages/composants/Avis.js
@@ -9,7 +9,7 @@ const Avis = () => {
             <div className='flex mx-auto w-fit'>
                 <a href="https://fr.trustpilot.com/review/lulusweetworld.shop" target='_blank'>
                 <Image src={tp} width={300} alt='trustpilot'/>
-                <p className='px-5'>TrustScore <span className=' font-bold'>4</span></p>
+                <p className='px-5'>TrustScore <span className=' font-bold'>4,1</span></p>
                 </a>
             </div>
         </div>
